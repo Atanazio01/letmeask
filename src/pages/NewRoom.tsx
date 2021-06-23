dc0a134a3965e661d18bafd4042419e72ddb460f
Marcos Douglas <marcosd.fernandes@uninove.edu.br>
2021-06-23 11:19:43 -0300

    direcionando o usuário para a sala criada

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import { FormEvent, useState } from "react";
 
 import illustrationImg from "../assets/images/illustration.svg";
@@ -13,6 +13,7 @@ import "../styles/auth.scss";
 
 export function NewRoom() {
   const { user } = useAuth();
+  const history = useHistory()
   const [newRoom, setNewRoom] = useState('');
 
   async function handleCreateRoom(event: FormEvent) {
@@ -28,6 +29,8 @@ export function NewRoom() {
       title: newRoom,
       authorId: user?.id,
     })
+
+    history.push(`/rooms/${firebaseRoom.key}`)
   }
 
   return (
